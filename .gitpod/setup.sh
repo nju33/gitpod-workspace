f679dfb90e4a3232c4f7535cd2a3432f01782977
純 <nju33.ki@gmail.com>
2021-04-17 18:35:53 +0000

    build: modify

---
+++ .gitpod/setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eu
+
+init_gpg() {
+  local secretkey="tmpsecretkey"
+  echo "$GPG_SECRET_KEY_NJU33" | base64 --decode >"$secretkey"
+
+  gpg --batch --import --allow-secret-key-import "$secretkey"
+
+  shred --remove "$secretkey"
+}
+
+init_bash() {
+  local parentdir="$HOME/.bashrc.d"
+  
+  if [ ! -d "$parentdir" ]; then
+    mkdir "$parentdir"
+  fi
+
+  echo "export GPG_TTY=\"$(tty)\"" >"$parentdir/gpg"
+}
+
+init_git() {
+  git config --global user.signingkey "$GIT_USER_SIGNINGKEY"
+  git config --global commit.gpgsign true
+}
+
+init_gpg
+init_bash
+init_git
