2fe092cafd6aaa19b1c923d3928977e137e89f56
純 <nju33.ki@gmail.com>
2021-04-19 01:07:53 +0900

    Update init.sh

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -2,8 +2,13 @@
 
 set -eux
 
+init_git() {
+  git config --global include.path="$HOME/.dotfiles/.gitconfig"
+}
+
 init_code() {
   code --install-extension "$HOME/wdhongtw.gpg-indicator-0.3.4.vsix"
 }
 
+init_git
 init_code
