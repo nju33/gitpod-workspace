0d476ce3627e4efd11bc795b27e2a51b1e1f6a26
純 <nju33.ki@gmail.com>
2021-04-19 17:28:57 +0900

    Update init.sh

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -6,10 +6,6 @@ init_git() {
   git config --global include.path="$HOME/.dotfiles/.gitconfig"
 }
 
-init_code() {
-  code --install-extension "$HOME/wdhongtw.gpg-indicator-0.3.4.vsix"
-}
-
 init_ngrok() {
   set +u
   if [ -n "$NGROK_AUTHTOKEN" ]; then
@@ -19,5 +15,4 @@ init_ngrok() {
 }
 
 init_git
-init_code
 init_ngrok
