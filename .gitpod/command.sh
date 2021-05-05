9829ab7504a5db1b967b5aa06293a6f1c28ccc0d
純 <nju33.ki@gmail.com>
2021-05-06 01:02:24 +0900

    Update command.sh

--- .gitpod/command.sh
+++ .gitpod/command.sh
@@ -28,9 +28,26 @@ init_gh() {
   set -ux
 }
 
+init_navi() {
+  local github_user="$(echo $GITPOD_WORKSPACE_CONTEXT | jq -r .repository.owner)"
+  local remote_url="https://github.com/$github_user/cheats.git"
+  local target_dir="$(navi info cheats-path)/${github_user}__cheats"
+  
+  if ! git ls-remote --exit-code "$remote_url" > /dev/null 2>&1; then
+    return
+  fi
+  
+  if [ -d "$target_dir" ]; then
+    cd "$target_dir" && git clone origin main
+  else
+    git clone "$remote_url" "$target_dir"
+  fi
+}
+
 init_git
 init_ngrok
 init_gh
+init_navi
 
 echo 'Please execute following like'
 echo '  init_code'
